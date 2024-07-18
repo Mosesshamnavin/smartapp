085474d9d077022dd5a7d9a5642d1d7c4bd8cb6f
Raja Singh <32429443+singhcool@users.noreply.github.com>
2024-07-18 19:15:06 +0530

    Update index.js

--- smart-analyzer/index.js
+++ smart-analyzer/index.js
@@ -2,24 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { server } = require("./config");
 const { postSentimentAnalyzer } = require("./src/routes");
-
+const cors = require('cors');
 const app = express();
 
-
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//app.post("/api/sentiment", postSentimentAnalyzer);
-
-app.post('/api/sentiment', (req, res) => {
-  console.log(req.body);
-  // ...
-});
+app.post("/api/sentiment", postSentimentAnalyzer);
 
 app.listen(server.port, server.host, () => {
   console.log(`Sentiment app listening on port ${server.port}`);
